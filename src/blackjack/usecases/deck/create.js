921d187dadfc404255494d7416ca9dfb9b11dc45
Octavio Cornejo <ocornejot@gmail.com>
2023-01-11 22:32:17 -0600

    Code refactor: Create Deck.

--- src/blackjack/usecases/deck/create.js
+++ src/blackjack/usecases/deck/create.js
@@ -1,32 +1,29 @@
 import { shuffle } from "underscore";
 
+let deck = [];
+const types = ['C', 'D', 'H', 'S'];
+const specials = ['A', 'J', 'Q', 'K'];
+
 /**
  * Creates a new deck.
- * @param {array<String>} cardTypes 
- * @param {array<String>} specialCards 
  * @returns {array<String>} returns a new deck.
  */
-export const createDeck = (cardTypes, specialCards) => {
-
-  if (!cardTypes || cardTypes === 0) 
-    throw new Error('carTypes is required.');
-
-  if (!specialCards || specialCards === 0) 
-    throw new Error('specialCards is required.');
-
-  let deck = [];
-
+const createDeck = () => {
+  //Creates the numeric cards for each card type.
   for (let i = 2; i <= 10; i++) {
-    for (let type of cardTypes) {
+    for (let type of types) {
       deck.push(i + type);
     }
   }
 
-  for (let type of cardTypes) {
-    for (let especial of specialCards) {
-      deck.push(especial + type);
+  //Creates the special cards for each special card.
+  for (let type of types) {
+    for (let special of specials) {
+      deck.push(special + type);
     }
   }
 
   return shuffle(deck);
 };
+
+export default createDeck;
